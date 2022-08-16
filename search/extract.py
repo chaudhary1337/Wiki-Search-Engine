d3d64eac23a5f888ec42d0567f87c05db8eebdc8
chaudhary1337 <tanishqchaudhary511@gmail.com>
2022-08-16 11:47:29 +0530

    Add: starting off the search

---
+++ search/extract.py
@@ -0,0 +1,68 @@
+from collections import defaultdict
+from misc import CUSTOM_STOPWORDS, FIELDS
+
+from nltk.corpus import stopwords
+import Stemmer
+
+
+class Extract:
+    def __init__(self):
+        self.nltk_stopwords = stopwords.words("english")
+        self.custom_stopwords = CUSTOM_STOPWORDS
+        self.stopwords = set(self.nltk_stopwords + self.custom_stopwords)
+        self.stemwords = Stemmer.Stemmer("english").stemWords
+
+    def clean(self, text):
+        tokens = text.split()
+        tokens_nonstop = [token for token in tokens if token not in self.stopwords]
+        tokens_stemmed = self.stemwords(tokens_nonstop)
+
+        return tokens_stemmed
+
+    def is_field_query(self, i, query):
+        if i < len(query) and query[i] in FIELDS and query[i + 1] == ":":
+            return query[i]
+        else:
+            return ""
+
+    def get_buffers_while(self, i, query):
+        buffer = []
+        while i < len(query) and not self.is_field_query(i, query):
+            buffer.append(query[i])
+            i += 1
+        return i, "".join(buffer).strip()
+
+    def parse(self, query):
+        i = 0
+        extracted = defaultdict(list)
+
+        while i < len(query):
+            # get the current field if exists, else ""
+            field = self.is_field_query(i, query)
+
+            # skipping the "X:" part, where X is the field
+            if field:
+                i += 2
+
+            # gets the new i and the buffer
+            i, buffer = self.get_buffers_while(i + 2, query)
+
+            # clean the buffer and add it
+            extracted[field].append(self.clean(buffer))
+
+        return extracted
+
+    def extract(self, query):
+        """
+        extracts all the information of a query
+        requires the vanilla keywords to be present at the start
+
+        returns a dictionary of {
+            field: list(list(stemmed keywords))
+        }
+
+        the field for vanilla keywords is ""
+        for the rest is the first word of the supported fields
+        """
+        extracted = self.parse(query)
+        return extracted
--- search/search.py
+++ search/search.py
@@ -1,1 +1,18 @@
-print("Running search.py")
+from misc import log
+from extract import Extract
+
+import sys
+
+
+@log
+def main():
+    path_to_inverted_index = sys.argv[1]
+    query = sys.argv[2]
+
+    extract = Extract()
+    extracted_query = extract.extract(query)
+    print("Hehe Boihhh")
+
+
+if __name__ == "__main__":
+    main()
